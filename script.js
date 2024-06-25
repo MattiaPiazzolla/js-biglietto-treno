a3ba7fc541385bd5e5dfe270d24d4af34aef9f19
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-25 20:44:39 +0200

    [FIX] fixed spelling error in the conditions

--- script.js
+++ script.js
@@ -5,15 +5,15 @@
     // Calculate the base price
     let price = km * 0.21;
     
-    let discountedPrice = price.toFixed(2)
+    let discountedPrice = price
     
     
     // Condition for the application of the discounts
     if (age < 18){
-         discountPrice = (price - (price / 100 * 20)).toFixed(2);
+        discountedPrice = (price - price / 100 * 20);
          document.getElementById('congratulationMsg').innerHTML = `<p>Congratulazioni hai diritto ad uno <span class="fw-bold">sconto</span> del <span class="text-success">20%</span></p>`;
     } else if (age > 65){
-         discountPrice = (price - (price / 100 * 40)).toFixed(2);
+        discountedPrice = (price - price / 100 * 40);
          document.getElementById('congratulationMsg').innerHTML = `<p>Congratulazioni hai diritto ad uno <span class="fw-bold">sconto</span> del <span class="text-success">40%</span></p>`;
     } 
     
