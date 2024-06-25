477ab2aae87623546ced957751dfc156e5f8b973
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-25 16:49:35 +0200

    [COMPLETED]

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 // Get data using prompt
 let km = prompt('Inserisci il numero di chilometri che devi percorrere');
 let age = prompt('Inserisci la tua età');
-let discountedPrice = 0
+
 // Calculate the base price
 let price = km * 0.21;
 
+let discountedPrice = price
 
 
 // Condition for the application of the discounts
 if (age < 18){
      discountPrice = price - (price / 100 * 20);
+     document.getElementById('congratulationMsg').innerHTML = `<p>Congratulazioni hai diritto ad uno <span class="fw-bold">sconto</span> del <span class="text-success">20%</span></p>`;
 } else if (age > 65){
      discountedPrice = price - (price / 100 * 40);
+     document.getElementById('congratulationMsg').innerHTML = `<p>Congratulazioni hai diritto ad uno <span class="fw-bold">sconto</span> del <span class="text-success">40%</span></p>`;
 } 
 
-document.getElementById('finalPrice').innerHTML = `Il prezzo del tuo biglietto è ${discountedPrice}€`;
+document.getElementById('finalPrice').innerHTML = `<p>Il prezzo del tuo biglietto è ${discountedPrice}€</p>`;
