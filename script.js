37bee49ebc84336b6d20ca1e85ebb6d63a3604d9
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-25 15:32:26 +0200

    [FEAT] define the three main variable (age km and price)

---
+++ script.js
@@ -0,0 +1,6 @@
+// Get data using prompt
+let km = prompt('Inserisci il numero di chilometri che devi percorrere');
+let age = prompt('Inserisci la tua età');
+
+// Calculate the base price
+let price = km * 0.21;
